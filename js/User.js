45e8784eea207801d7d0e1307715969ed19c87a9
wildanzm <wildanmanapi123@gmail.com>
2024-04-20 07:24:40 +0700

    feat : save data to localstorage

--- js/User.js
+++ js/User.js
@@ -1,9 +1,23 @@
 class User {
+	constructor() {
+		this.users = this.getUsers() || [];
+	}
+
 	saveUser(userData) {
 		const newUser = {
 			id: Date.now(),
 			...userData,
 		};
+
+		this.users.push(newUser);
+		localStorage.setItem('users', JSON.stringify(this.users));
+
+		return window.location = '../signin.html'
+
       console.log(userData)
+	}
+
+	getUsers() {
+		return JSON.parse(localStorage.getItem('users')) || [];
 	}
 }
